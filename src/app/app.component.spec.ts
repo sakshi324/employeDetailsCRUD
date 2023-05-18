325d75334848363ecbaf7cdbbaeedb2d9f65b2c9
sakshi324 <sakshipatil1900@gmail.com>
2023-05-18 23:14:23 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'employeDetails'`, () => {
+  it(`should have as title 'crud-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employeDetails');
+    expect(app.title).toEqual('crud-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('employeDetails app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('crud-app app is running!');
   });
 });
